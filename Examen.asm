4922dd1a4b0454994f41842c73833ec7c4d74e66
Daniel Alonso <dalonsobascones@gmail.com>
2020-02-09 15:00:01 +0100

    Add files via upload

---
+++ Examen.asm
@@ -0,0 +1,35 @@
+;FECHA: 1 de Junio 2017 
+;Enunciado: Rutina en el simulador que cuando pulses un numero se represente en la pantalla, se repite hasta que pulses el 0 y el programa termine. Interrupcion 5.5
+
+.ORG 1200H
+
+MVI A, 08H ; Inicializa A, a mascara de interrupciones
+MVI B, FFH ; Inicializa B
+MVI C, FFH ; Inicializa C
+LXI SP, 1200H ;pila
+REP: 
+	EI
+	RIM
+	CALL 2CH 
+	JMP REP
+
+
+;Teclado
+.ORG 2CH ; Interrupción 5.5
+IN 00
+CPI 3AH
+SUI 30H
+CPI 30H
+HLT
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ P1.asm
@@ -0,0 +1,47 @@
+.ORG 100AH
+; ------------
+;  Ejercicio 1
+; ------------
+MVI A, 1H
+MVI B, 2H
+MVI C, 3H
+MVI D, 4H
+MVI E, 5H
+MVI H, 6H
+MVI L, 7H
+; ------------
+;  Ejercicio 2
+; ------------
+MOV M, A
+; ------------
+;  Ejercicio 3
+; ------------
+INX H
+MOV M, B
+INX H
+MOV M, C
+INX H
+MOV M, D
+INX H
+MOV M, E
+; ------------
+;  Ejercicio 4
+; ------------
+SHLD 60FH
+; ------------
+;  Ejercicio 5
+; ------------
+XCHG
+MOV M, A
+; ------------
+;  Ejercicio 6
+; ------------
+LHLD 60FH
+MOV M, C
+; ------------
+;  Ejercicio 7
+; ------------
+STAX B
+MVI A, 44H
+STAX D
+HLT 
